04b7d74c00ff724ea571defaf462501738b20050
embchnk <baniak996@gmail.com>
2018-10-25 23:51:06 +0200

    Initial config

---
+++ server/server.wsgi
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+import sys
+sys.path.insert(0, "/home/embchnk/startApp")
+from server import app as application
---
+++ static/js/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <p> Hello React!</p>
+                <a href="../test.html">Test</a>
+            </div>
+        );
+    }
+}
