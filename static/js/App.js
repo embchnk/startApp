77a712139bf68b3cb034aa2b66e6d0040163f2de
embchnk <baniak996@gmail.com>
2018-10-26 20:42:38 +0200

    Made page with antd

--- static/js/App.js
+++ static/js/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { Input, Button } from 'antd';
 
 export default class App extends React.Component {
     render() {
         return (
             <div>
                 <p> Hello React!</p>
-                <a href="../test.html">Test</a>
+                <Button> ...and Ant Design Button!</Button>
+                <Input placeholder="Basic usage" />
             </div>
         );
     }
--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,3 +1,11 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./index.html",
+    filename: "./index.html"
+});
+
 const webpack = require('webpack')
 
 const config = {
@@ -17,9 +25,38 @@ const config = {
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: 'style-loader' // creates style nodes from JS strings
+                    },
+                    {
+                        loader: 'css-loader' // translates CSS into CommonJS
+                    },
+                    {
+                        loader: 'less-loader', // compiles Less to CSS
+                        options: {
+                            javascriptEnabled: true
+                        }
+                    }
+                ]
             }
         ]
-    }
+    },
+    plugins: [htmlPlugin]
 };
 
 module.exports = config;
